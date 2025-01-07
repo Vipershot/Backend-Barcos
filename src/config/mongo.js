67bc2a0f04949002bfb1e336e099c6dabd4fd221
Vipershot <imvipershot@gmail.com>
2025-01-07 15:11:20 -0500

    ts-integration

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const dbConnect = () => {
-  const DB_URI = process.env.DB_URI;
+  const DB_URI = "mongodb+srv://back-ships20:Z8GbOOlW7U4jUWwA@cluster0.wec6tee.mongodb.net/";
   mongoose
     .connect(DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then(() => console.log(`*** CONEXION EXITOSA ***`))
-    .catch(() => console.log(`*** ERROR DE CONEXION ***`));
+    .catch((error) => console.log(error));
 };
 
 module.exports = dbConnect;
